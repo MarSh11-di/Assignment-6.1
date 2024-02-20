4e14ddc45f30ef90d383d78711a76635ecb1bd9a
Mariia Sheva <mariya.shevchenko.@gmail.com>
2024-02-20 20:15:19 +0100

    added multiplication function

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ parser = argparse.ArgumentParser(description='calculator addition')
 def add(x, y):
    return x+y
 def subtract(x, y):
-    return x-y
+   return x-y
+def multiply(x, y):
+   return x*y
 
-parser.add_argument("operation",  choices= ["add","subtract"])
+parser.add_argument("operation",  choices= ["add","subtract","multiply"])
 parser.add_argument("x", type=float, help="The first number x")
 parser.add_argument("y", type=float, help="The second number y")
 args = parser.parse_args()
 if args.operation == "add":
    result = add(args.x, args.y)
-else:
+elif args.operation == "subtract":
    result = subtract(args.x, args.y)
+elif args.operation == "multiply":
+   result = multiply(args.x, args.y)
+
 
 print("Result:", result)
