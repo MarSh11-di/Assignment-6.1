b727c6c97eccfb95478c1366278d3f04db639b27
Mariia Sheva <mariya.shevchenko.@gmail.com>
2024-02-22 13:20:38 +0100

    added division function

--- app.py
+++ app.py
@@ -3,15 +3,23 @@ parser = argparse.ArgumentParser(description='calculator addition')
 def add(x, y):
    return x+y
 def subtract(x, y):
-    return x-y
+   return x-y
+def division(x,y):
+   if y == 0:
+      print("You cannot divide by 0") 
+      exit()
+   return round((x/y),2)
 
-parser.add_argument("operation",  choices= ["add","subtract"])
+
+parser.add_argument("operation",  choices= ["add","subtract","division"])
 parser.add_argument("x", type=float, help="The first number x")
 parser.add_argument("y", type=float, help="The second number y")
 args = parser.parse_args()
 if args.operation == "add":
    result = add(args.x, args.y)
-else:
+elif args.operation == "subtract":
    result = subtract(args.x, args.y)
+elif args.operation == "division":
+   result = division(args.x, args.y)
 
 print("Result:", result)
